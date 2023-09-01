7dc36cefcf160516d754471160e8ef99149fdc7c
unknown <mdirfan29pro@gmail.com>
2023-09-01 06:36:48 +0530

    new-6

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ require("./db/conn");
 const router = require("./routes/router");
 const cors = require("cors");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
